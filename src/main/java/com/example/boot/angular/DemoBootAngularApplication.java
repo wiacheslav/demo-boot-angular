79e651f6b810a5406e3771f0c95a120d83a28065
Viacheslav <scrynkaa@gmail.com>
2018-01-06 12:48:19 +0200

    initial commit

---
+++ src/main/java/com/example/boot/angular/DemoBootAngularApplication.java
@@ -0,0 +1,12 @@
+package com.example.boot.angular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBootAngularApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoBootAngularApplication.class, args);
+    }
+}
